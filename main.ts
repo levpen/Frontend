410eeea910aecb9547215e5dfcf54075239b6fbe
levpen <levpen@yandex.ru>
2022-06-16 15:05:01 +0300

    Groomed the code

--- main.ts
+++ main.ts
@@ -2,13 +2,13 @@ let apiFrom: HTMLSelectElement = document.getElementById('apiFrom') as HTMLSelec
 let apiTo: HTMLSelectElement = document.getElementById('apiTo') as HTMLSelectElement
 
 fetch(`https://open.er-api.com/v6/latest/RUB`)
-.then(res => res.json())
-.then((data) => {
-    for(let i in data.rates){
-        apiFrom.innerHTML += `<option> ${i} </option>`
-        apiTo.innerHTML += `<option> ${i} </option>`
-    }
-});
+    .then(res => res.json())
+    .then((data) => {
+        for (let i in data.rates) {
+            apiFrom.innerHTML += `<option> ${i} </option>`
+            apiTo.innerHTML += `<option> ${i} </option>`
+        }
+    });
 
 const apiForm = document.getElementById('apiForm') as HTMLFormElement
 const apiResult = document.getElementById('apiResult') as HTMLDivElement
@@ -20,15 +20,15 @@ function addPost(e) {
     const fromCurr: string = apiFrom.value;
     const toCurr: string = apiTo.value;
     fetch(`https://open.er-api.com/v6/latest/${fromCurr}`)
-    .then((res) => res.json())
-    .then((data) => {
-        let convertedAmount = amount;
-        for (let i in data.rates) {
-            if(i === toCurr){
-                convertedAmount *= data.rates[i];
-                console.log(data.rates[i]);
+        .then((res) => res.json())
+        .then((data) => {
+            let convertedAmount = amount;
+            for (let i in data.rates) {
+                if (i === toCurr) {
+                    convertedAmount *= data.rates[i];
+                    console.log(data.rates[i]);
+                }
             }
-        }
-        (apiResult as HTMLElement).innerHTML = `<p>${amount} ${fromCurr} = ${convertedAmount} ${toCurr}</p>`
-    });
+            (apiResult as HTMLElement).innerHTML = `<p>${amount} ${fromCurr} = ${convertedAmount} ${toCurr}</p>`
+        });
 }
